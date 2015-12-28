f6c4ee99a76c66aad7d64fabdd82ecd63ad12499
Taylor Lipson <tlipson@purdue.edu>
2015-12-27 20:31:38 -0500

    Gateway
    The gateway had originally set the player animator to change state even
    if it wasn’t in the trigger zone, so by changing the gateway to rely on
    the oncollisionidle object and script it fixed that bug.

--- Archer Dude/assets/prefabs/Puzzles/Gateway/GateStop.cs
+++ Archer Dude/assets/prefabs/Puzzles/Gateway/GateStop.cs
@@ -5,24 +5,17 @@ public class GateStop : MonoBehaviour {
 
 	private Animator animatorGate;			//Gate animator
 	private Animator animatorPlayer;		//Player animator
+	private GameObject enemyStop;					//enemy stopper for this puzzle
 	public GameObject target;				//the target that triggers animation (will set this later to be it's own class for a list of targets)
+
 
 	// Use this for initialization
 	void Start () 
 	{
 			//getting components
 		animatorGate = GetComponent<Animator>();
-		GameObject player = GameObject.Find ("Player");
-		animatorPlayer = player.GetComponent<Animator>();
-	}
-
-		//if the player collides, stop it from moving.
-	void OnTriggerEnter2D(Collider2D coll)
-	{
-		if (coll.gameObject.tag == "Player" && animatorGate.GetBool("Closed") == true)
-		{
-			animatorPlayer.SetBool("run", false);
-		}
+		enemyStop = this.transform.FindChild("oncollisionidle").gameObject;
+		enemyStop.GetComponent<OnCollisionIdle>().stop = true;
 	}
 
 		//keep checking to see if the target has been hit, when it is hit, open the gate
@@ -32,12 +25,7 @@ public class GateStop : MonoBehaviour {
 		{
 			animatorGate.SetBool ("Closed", false);
 			animatorGate.SetBool ("Open", true);
+			enemyStop.GetComponent<OnCollisionIdle>().stop = false;	
 		}
-	}
-
-		//called by the open animation, tells the player to start running again.
-	void Go()
-	{
-		animatorPlayer.SetBool ("run", true);
 	}
 }

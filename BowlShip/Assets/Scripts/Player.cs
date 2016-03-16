59eda4686705d5fa45332c0cd590d1579e0f508d
Michael Parr <trumpetdude213@yahoo.com>
2016-03-16 03:06:40 -0400

    Input Axis

--- BowlShip/Assets/Scripts/Player.cs
+++ BowlShip/Assets/Scripts/Player.cs
@@ -3,13 +3,26 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
-	// Use this for initialization
+	public int playerNum;								//The player controlling this player
+	int health;											//How much health the ship has left
+	int shield;											//How much shields the ship has
+	float speed;										//How fast the ship can accelerate
+	float fireRate;										//How fast the ship can fire
+	float horiz;										//The horizontal movement input
+	float vert;											//The vertical movement input
+	Weapon[] weapons;									//An array of collected weapons
+
+	/** Use this for initialization
+	 * 
+	 * */
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		
+
+
 	}
 }

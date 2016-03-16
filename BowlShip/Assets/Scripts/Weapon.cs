1ac5b6ebf57cea021afeb164af045d9bb845bc10
Michael Parr <trumpetdude213@yahoo.com>
2016-03-16 06:42:40 -0400

    Fix the dumb git glitch

--- BowlShip/Assets/Scripts/Weapon.cs
+++ BowlShip/Assets/Scripts/Weapon.cs
@@ -5,13 +5,8 @@ public class Weapon : Collectable {
 
 	public float timer = 5.0f;			//if isTimer is false, this is the quantity of bullets of the weapon, else it is a timer
 	public bool isTimer = false;		//^
-<<<<<<< HEAD
 	public float fireRate;				//how fast this weapon can fire (Set in each individual weapon)
-	
-=======
-	public float fireRate;				//how fast this weapon can fire (Set in each individual weapon)	
 
->>>>>>> origin/master
 /// <summary>
 /// if the player collides with this object, the player picks up this object.
 /// picking up entails making this object no longer collidable or visible

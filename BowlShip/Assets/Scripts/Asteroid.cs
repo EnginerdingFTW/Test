b687d43b9666adc55ce5e1b02f65e4617cdff382
Taylor Lipson <tlipson@purdue.edu>
2016-03-16 06:39:35 -0400

    committing for the "night"

--- BowlShip/Assets/Scripts/Asteroid.cs
+++ BowlShip/Assets/Scripts/Asteroid.cs
@@ -3,14 +3,19 @@ using System.Collections;
 
 public class Asteroid : MonoBehaviour {
 
-	public float powerupChance = 0.5f;	
-	public float rotateSpeed = 1.0f;
-	public GameObject bigAsteroid;
-	public float damage = 30.0f;
+	public GameObject bigAsteroid;				//big asteroid prefab
+	public GameObject smallAsteroid;			//smal ".."
+	public float powerupChance = 0.5f;			//probability that a big asteroid drops a powerup
+	public float rotateSpeed = 1.0f;			//rotation speed of the asteroid
+	public float damage = 30.0f;				//damage done to player if hit
+	public float smallAstDamageRatio = 0.33f;	//ratio of small asteroid damage to large
+	public float spawnSpeed = 5.0f;				//velocity of small asteroids spawned
+	public int numberOfSmallAsteroids = 3;		//number of small asteroids maximum spawned when big one destroyed
 
 	// Use this for initialization
 	void Start () 
 	{
+		this.transform.parent = GameObject.Find("Asteroids").transform;
 		rotateSpeed *= Random.Range(0.0f, 1.0f);
 	}
 	
@@ -22,15 +27,26 @@ public class Asteroid : MonoBehaviour {
 
 	void OnCollisionEnter2D(Collision2D coll)
 	{
+		//if the asteroid is being destroyed....check if this is a big asteroid, if so spawn small ones and possibly a powerup
 		if (coll.collider.tag == "WeaponFire")
 		{
 			if (bigAsteroid == this.gameObject)
 			{
 				if (Random.Range(0.0f, 1.0f) >= powerupChance)
 				{
-					//instantiate powerup
+					//instan
 				}
 				//instantiate small asteroids spreading out.
+				int k = 0;
+				int count = Random.Range(1, numberOfSmallAsteroids);
+				for (k = 0; k < count; k++)
+				{	
+					Vector3 newPos = this.transform.position + new Vector3(Random.Range(-1.5f, 1.5f), Random.Range(-1.5f, 1.5f), 0.0f);
+					GameObject temp = Instantiate(smallAsteroid, newPos, Quaternion.identity) as GameObject;
+					Vector3 vel = newPos - this.transform.position;
+					temp.GetComponent<Rigidbody2D>().velocity = spawnSpeed * new Vector2(vel.x, vel.y);
+				}
+				Destroy(this.gameObject);
 			} 
 			else 
 			{
@@ -38,15 +54,16 @@ public class Asteroid : MonoBehaviour {
 			}
 		}
 
+		//if the asteroid collides with the player hurt the player
 		if (coll.collider.tag == "Player")
 		{
 			if (bigAsteroid == this.gameObject)
 			{
-				//coll.collider.gameObject.GetComponent<Player>().
+				coll.collider.gameObject.GetComponent<Player>().Hurt((int)damage);
 			}	
 			else
 			{
-
+				coll.collider.gameObject.GetComponent<Player>().Hurt((int)(damage * smallAstDamageRatio));
 			}	
 		}
 	}

c10a64baa3505a41490f865ae93ab6391d883d99
Michael Parr <trumpetdude213@yahoo.com>
2016-03-16 06:07:22 -0400

    Finished Player

--- BowlShip/Assets/Scripts/Weapon.cs
+++ BowlShip/Assets/Scripts/Weapon.cs
@@ -5,13 +5,7 @@ public class Weapon : Collectable {
 
 	public float timer = 5.0f;			//if isTimer is false, this is the quantity of bullets of the weapon, else it is a timer
 	public bool isTimer = false;		//^
-<<<<<<< HEAD
-	public float fireRate;				//how fast this weapon can fire (Set in each individual weapon)
-=======
-
-
->>>>>>> origin/master
-	
+	public float fireRate;				//how fast this weapon can fire (Set in each individual weapon)	
 
 /// <summary>
 /// if the player collides with this object, the player picks up this object.

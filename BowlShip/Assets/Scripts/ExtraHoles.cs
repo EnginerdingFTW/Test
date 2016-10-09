954b237f48e6e54763ddf8fc7d0e00753ab84c27
Michael Parr <trumpetdude213@yahoo.com>
2016-10-09 18:02:07 -0400

    Updated WarpHoles
    It now has twice the amount of warp holes with 5 or more players. Very
    Satisfying

---
+++ BowlShip/Assets/Scripts/ExtraHoles.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtraHoles : MonoBehaviour {
+
+	private SceneController sc;				//the scenecontroller
+	public GameObject extra;				//the extra warpholes
+
+	// Use this for initialization
+	void Start () {
+		sc = GameObject.FindGameObjectWithTag ("SceneController").GetComponent<SceneController>();
+		if (sc.numPlayers > 4) {
+			extra.SetActive (true);
+		}
+	}
+}
--- BowlShip/Assets/Scripts/WarpHolePowerUps.cs
+++ BowlShip/Assets/Scripts/WarpHolePowerUps.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class WarpHolePowerUps : MonoBehaviour {
 
+	public int portalLoc = 0;							//0 for right, 1 for topLeft, 2 for bottomLeft
 	public float timeTillSpawn = 15.0f;					//How much time until a powerup is spawned
 	public GameObject[] powerUps;						//One of the powerUps that can be spawned
 	private int powerUpToSpawn;							//The number associated with the spawned powerup
@@ -19,7 +20,17 @@ public class WarpHolePowerUps : MonoBehaviour {
 	IEnumerator SpawnPowerUp () {
 		yield return new WaitForSeconds (timeTillSpawn);
 		powerUpToSpawn = Random.Range(0, powerUps.Length);
-		Instantiate (powerUps [powerUpToSpawn], new Vector3(transform.position.x - 0.1f, transform.position.y, -1.0f), Quaternion.identity);
+		switch (portalLoc) {
+		case 0:
+			Instantiate (powerUps [powerUpToSpawn], new Vector3 (transform.position.x - 0.1f, transform.position.y, -1.0f), Quaternion.identity);
+			break;
+		case 1:
+			Instantiate (powerUps [powerUpToSpawn], new Vector3 (transform.position.x + 0.08f, transform.position.y - 0.1f, -1.0f), Quaternion.identity);
+			break;
+		case 2:
+			Instantiate (powerUps [powerUpToSpawn], new Vector3 (transform.position.x + 0.08f, transform.position.y + 0.1f, -1.0f), Quaternion.identity);
+			break;
+		}
 		StartCoroutine ("SpawnPowerUp");
 	}
 }

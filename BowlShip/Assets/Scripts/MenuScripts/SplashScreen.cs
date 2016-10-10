ebfaa73edf4e86d9cbd649b1d1caf0227b3dd58a
Michael Parr <trumpetdude213@yahoo.com>
2016-10-10 13:31:22 -0400

    Basic ParrTay Gaming splashscreen

---
+++ BowlShip/Assets/Scripts/MenuScripts/SplashScreen.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour {
+
+	/// <summary>
+	/// Logo animation ends by calling the main menu up
+	/// </summary>
+	public void Play () {
+		SceneManager.LoadScene ("Menu");
+	}
+}
